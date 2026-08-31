1a49dbaa37eb3948b5448826c87062c57f2c47e0
RoLo <rolodoom@gmail.com>
2026-08-31 17:16:41 -0500

    :update: transaltions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,14 @@ export default function Footer({ lang }) {
     <footer className="mt-6 flex items-center justify-center gap-1 text-center text-sm text-gray-500 dark:text-gray-400">
       <Code className="h-4 w-4 text-indigo-500 dark:text-indigo-400" />{' '}
       {t(lang, 'footer')}{' '}
+      <a
+        href="https://github.com/rolodoom/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-indigo-400 transition-colors duration-200 hover:text-indigo-300 dark:text-indigo-400 dark:hover:text-indigo-300"
+      >
+        RoLoDoOm
+      </a>
       <PenTool className="h-4 w-4 text-indigo-500 dark:text-indigo-400" />
     </footer>
   )
